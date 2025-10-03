9d90cec592151d12cf8bbfb4789d9dddee2ca179
RecLxss <goldfistone@gmail.com>
2025-10-03 23:09:07 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/ZeroScreen/", // 👈 Lägg till detta!
   server: {
     host: "::",
     port: 8080,
